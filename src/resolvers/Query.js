d848dc43aac3722fdbca4be2ddcd1a8cb20b808d
ivallee <isaacvallee@gmail.com>
2018-02-16 11:33:12 -0800

    implemet getUserByID query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,12 +1,19 @@
 function allUsers(parent, args, ctx, info) {
-  return ctx.db.query.users({}, info)
+  return ctx.db.query.users({}, info);
+}
+
+function getUserById(parent, args, ctx, info) {
+  const { userId } = args;
+
+  return ctx.db.query.user({ where: { id: userId } }, info);
 }
 
 function allGroups(parent, args, ctx, info) {
-  return ctx.db.query.groups({}, info)
+  return ctx.db.query.groups({}, info);
 }
 
 module.exports = {
   allUsers,
-  allGroups
+  allGroups,
+  getUserById
 }
