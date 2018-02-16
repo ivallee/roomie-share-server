9c7afa4994e284608a1bff8b99b471de0b78c03e
ivallee <isaacvallee@gmail.com>
2018-02-16 13:59:34 -0800

    implement login mutation and auth

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const server = new GraphQLServer({
     db: new Prisma({
       typeDefs: 'src/generated/prisma.graphql',
       endpoint: 'http://localhost:4466/server/dev', // the endpoint of the Prisma DB service
-      secret: process.env.PRISMA_SECRET, // specified in database/prisma.yml
+      secret: process.env.PRISMA_SECRET,
       debug: true, // log all GraphQL queryies & mutations
     }),
   }),
--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
-
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+
+const { APP_SECRET } = require('../utils');
 
 async function signup(parent, args, ctx, info) {
   const password = await bcrypt.hash(args.password, 10);
@@ -17,6 +17,27 @@ async function signup(parent, args, ctx, info) {
   }
 }
 
+async function login(parent, args, ctx, info) {
+  const user = await ctx.db.query.user({ where: { email: args.email } });
+
+  if (!user) {
+    throw new Error(`Could not find a user with email: ${args.email}`);
+  }
+
+  const valid = await bcrypt.compare(args.password, user.password);
+  if (!valid) {
+    throw new Error('Invalid login');
+  }
+
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+  return {
+    token,
+    user,
+  }
+}
+
 module.exports = {
-  signup
+  signup,
+  login
 }
