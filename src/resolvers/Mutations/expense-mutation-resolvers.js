276c9c9dbea0abc273c43d940e3a004c880faa8b
ivallee <isaacvallee@gmail.com>
2018-02-20 15:29:29 -0800

    add validation for group membership

--- src/resolvers/Mutations/expense-mutation-resolvers.js
+++ src/resolvers/Mutations/expense-mutation-resolvers.js
@@ -3,8 +3,19 @@ const { validateUser, calculatePerPerson } = require('../../utils');
 async function newExpense(parent, args, ctx, info) {
   const { groupId, amount, description, participants } = args;
   const userId = validateUser(ctx);
-
   const participantShares = [];
+
+  const validateCreator = await ctx.db.exists.Group({
+    id: groupId,
+    users_some: {
+      id: userId
+    }
+  });
+
+  if (!validateCreator) {
+    throw new Error('You cannot add expenses to a group you do not belong to.');
+  }
+
 
   const newParticipants = participants.map((p) => {
     participantShares.push(p.share);
