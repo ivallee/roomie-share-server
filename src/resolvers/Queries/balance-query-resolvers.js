e09f48ee922a50d03fa645a808fea728c2d02f74
ivallee <isaacvallee@gmail.com>
2018-02-23 17:32:38 -0800

    change fields returned from resolver, adjust schema, cleanup hanging consolelogs

--- src/resolvers/Queries/balance-query-resolvers.js
+++ src/resolvers/Queries/balance-query-resolvers.js
@@ -29,6 +29,7 @@ async function balance(parent, args, ctx, info) {
 
   let totalPaid = [];
   let totalOwed = 0;
+  
   expenses.forEach((e) => {
     if (e.paidBy.id === userId) {
       totalPaid.push(e.amount);
@@ -37,31 +38,17 @@ async function balance(parent, args, ctx, info) {
     e.participants.forEach((p) => {
       if (p.user.id === userId) {
         const sumProduct = p.share * e.perPerson;
-        console.log(sumProduct);
         totalOwed += sumProduct;
       }
     });
   });
   totalPaid = totalPaid.reduce((acc, curr) => acc + curr);
 
-
-
-  console.log('DAATTTAAAAAA', expenses[0].participants, expenses[0].paidBy);
-  console.log("AAAAAMAOUUUUNT ", totalPaid, totalOwed)
-
-  // figure out which ones user paid for
-
-  // function to calculate total paid
-
-  // function to calculate total owed
-
-  // TODO: add check to see if expense has been "paid"?
-
-
   return {
-    totalPaid,
-    totalOwed,
-    balance: totalPaid - totalOwed
+    userId,
+    totalPaid: Math.round(totalPaid),
+    totalOwed: Math.round(totalOwed),
+    balance: Math.round(totalPaid - totalOwed)
   }
 }
 
